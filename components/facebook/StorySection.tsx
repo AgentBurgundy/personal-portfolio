3778ddb4df2250e67c6392c6aaa98d28fa6138af
AgentBurgundy <professional.burgundy@gmail.com>
2022-01-30 00:47:11 -0600

    minor bug fix

--- components/facebook/StorySection.tsx
+++ components/facebook/StorySection.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import Image from 'next/Image'
+import Image from 'next/image'
 import famyshImage from '../../assets/famysh-story.png'
 
 export default function StorySection() {
